0083ae8ef632d2e3b6400b78adff7424908bebdd
Daniel Ohrin <xqiam@outlook.com>
2022-11-29 13:59:46 -0600

    PhaseTwo Complete

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BankHeist
 {
@@ -19,11 +20,42 @@ namespace BankHeist
             Console.Write("Enter a courage factor: ");
             teamMemberOne.CourageFactor = double.Parse(Console.ReadLine());
 
-            Console.WriteLine($@"Team Member One:
-            Name: {teamMemberOne.Name}
-            Skill Level: {teamMemberOne.SkillLevel}
-            Courage Factor: {teamMemberOne.CourageFactor}");
+            List<Member> teamMembers = new List<Member> { teamMemberOne };
 
+            // Loops until user enters an empty string for name
+            while (true == true)
+            {
+                Console.Write("Enter a name: ");
+                string name = Console.ReadLine();
+
+                // If name entered is an empty string, break out of loop and don't prompt for more info
+                if (name == "")
+                {
+                    break;
+                }
+
+                // Create a new instance of Member class
+                Member newTeamMember = new Member { Name = name };
+
+                Console.Write("Enter a skill level: ");
+                newTeamMember.SkillLevel = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter a courage factor: ");
+                newTeamMember.CourageFactor = double.Parse(Console.ReadLine());
+
+                teamMembers.Add(newTeamMember);
+            }
+
+            Console.WriteLine($"There are {teamMembers.Count} team members.");
+
+            // Displays each team member's information -- weird indentation because @ symbol is super specific syntax 
+            foreach (Member teamMember in teamMembers)
+            {
+                Console.WriteLine($@"Team Member:
+    Name: {teamMember.Name}
+    Skill Level: {teamMember.SkillLevel}
+    Courage Factor: {teamMember.CourageFactor}");
+            }
         }
     }
 }
